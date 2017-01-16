44ce91acd3935aa4651f5cd037fc2bc18bb8bb9b
VladiMakarevich <justadreampictures@gmail.com>
2017-01-16 11:27:55 +0300

    Create CommandChatApp

---
+++ Command.ChatApp/Exceptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Command.ChatApp
+{
+    /// <summary>
+    /// Throw when the remote server not found.
+    /// </summary>
+    public class ServerNotFoundException : Exception
+    {
+        /// <summary>
+        /// Creates an instance of ServerNotFoundException class.
+        /// </summary>
+        /// <param name="message">The message to show to the user.</param>
+        public ServerNotFoundException(string message) : base(message)
+        { }
+    }
+}
